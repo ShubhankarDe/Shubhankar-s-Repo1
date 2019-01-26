e4862dffb810d40fccdd0e5815f804fd4dfd336c
SAhubhankarDe <de.subho9@gmail.com>
2019-01-26 23:17:45 +0530

    Commit chnages for Jenkins

---
+++ TestAutomation/src/test/java/com/automation/runner/CucumberRunnerTest.java
@@ -0,0 +1,29 @@
+package com.automation.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		
+		features= {"src/test/resources/MyFeature1.feature"},
+				 plugin = { 
+						    "html:target/cucumber-html-report",
+						    "json:target/cucumber.json"
+						        },
+				 glue = {"StepDef"},strict = true,
+				   dryRun= false, monochrome = true, snippets= SnippetType.CAMELCASE)
+		
+		  
+public class CucumberRunnerTest {
+		
+
+		
+	 
+
+}
--- TestAutomation/src/test/java/com/automation/utilities/SeleniumGrid.java
+++ TestAutomation/src/test/java/com/automation/utilities/SeleniumGrid.java
@@ -63,6 +63,7 @@ public class SeleniumGrid extends ActionMethods{
 		driver.quit();			
 	}
 	
+	
 
 
 
